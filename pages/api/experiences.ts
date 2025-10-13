85ed6dc70370db2e57ffc1e0648ab130aee60436
Matt Thomas <matt@Matts-MacBook-Pro.local>
2025-10-13 18:13:21 +1100

    fix: test experiences API route

--- pages/api/experiences.ts
+++ pages/api/experiences.ts
@@ -1,3 +1,10 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({ ok: true, message: 'API route working' });
+}
+
+
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
